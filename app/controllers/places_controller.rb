0fcf808162fc33d1f90a69485dfa2be462629849
vagrant <vagrant@precise32.(none)>
2015-10-24 05:21:15 +0000

    Lesson 27 Validation

--- app/controllers/places_controller.rb
+++ app/controllers/places_controller.rb
@@ -10,8 +10,13 @@ class PlacesController < ApplicationController
   end
 
   def create
-    current_user.places.create(place_params)
-    redirect_to root_path
+    @place = current_user.places.create(place_params)
+
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def show
@@ -34,7 +39,12 @@ class PlacesController < ApplicationController
     end
 
     @place.update_attributes(place_params)
-    redirect_to root_path
+
+    if @place.valid?
+      redirect_to root_path
+    else
+      render :new, :status => :unprocessable_entity
+    end
   end
 
   def destroy
@@ -43,7 +53,7 @@ class PlacesController < ApplicationController
     if @place.user != current_user
       return render :text => 'Not Allowed', :status => :forbidden
     end
-    
+
     @place.destroy
     redirect_to root_path
   end
